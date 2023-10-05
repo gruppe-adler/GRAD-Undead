d34036174e98d651a97c1c62ad04b67728dae646
y0014984 <y0014984@gmail.com>
2023-10-05 10:07:42 +0200

    added unconsciousness and leg damage to collisions like falling down

--- scripts/Game/Components/Damage/GRAD_CharacterDamageManagerComponent.c
+++ scripts/Game/Components/Damage/GRAD_CharacterDamageManagerComponent.c
@@ -26,6 +26,72 @@ modded class SCR_CharacterDamageManagerComponent : ScriptedDamageManagerComponen
 			}
 		}
 
+		// if damage type is 'collision' which could be for example fall damage
+		// the the damage is also applied on all 6 leg parts to create treatable injuries
+		
+		if (type == EDamageType.COLLISION)
+		{
+			array<HitZone> hitZones = {};
+			GetAllHitZones(hitZones);
+			
+			foreach (HitZone hitZone : hitZones)
+			{
+				float fallDamage;
+				
+				switch (hitZone.GetName())
+				{
+					case "RThigh":
+						fallDamage = damage * 0.1;
+						break;
+					case "LThigh":
+						fallDamage = damage * 0.1;
+						break;
+					case "RCalf":
+						fallDamage = damage * 0.3;
+						break;
+					case "LCalf":
+						fallDamage = damage * 0.3;
+						break;
+					case "RFoot":
+						fallDamage = damage * 0.1;
+						break;
+					case "LFoot":
+						fallDamage = damage * 0.1;
+						break;
+					default:
+						break;
+				}
+				
+				float health = hitZone.GetHealth();
+				
+				float newHealth = health - fallDamage;
+				
+				if (newHealth <= 0)
+					hitZone.SetHealthScaled(0.01);
+				else
+					hitZone.SetHealth(newHealth);
+				
+				// if the collision damage is above threshold then
+				// the character gets unconscious for a certain amount of time
+				
+				// a damage of 25 is equal to a fall from a height of roughly 8-10 meters
+				if (damage > 25)
+				{
+					ChimeraCharacter character = ChimeraCharacter.Cast(GetOwner());
+					if (!character)
+						return;
+					
+					CharacterControllerComponent controller = character.GetCharacterController();
+					if (!controller)
+						return;
+		
+					controller.SetUnconscious(true);
+					
+					// wake up after 15 sec
+					GetGame().GetCallqueue().CallLater(controller.SetUnconscious, 15000, false, false);
+				}
+			}
+		}
 		
 		super.OnDamage(type, damage, pHitZone, instigator, hitTransform, speed, colliderID, nodeID);
 	}
