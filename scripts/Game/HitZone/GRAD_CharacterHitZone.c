ef5cfbb9f53f1f4c6fd76461059fa98450cdf660
y0014984 <y0014984@gmail.com>
2023-10-17 11:11:29 +0200

    added check for units controlled by game master

--- scripts/Game/HitZone/GRAD_CharacterHitZone.c
+++ scripts/Game/HitZone/GRAD_CharacterHitZone.c
@@ -10,7 +10,14 @@ modded class SCR_RegeneratingHitZone : ScriptedHitZone
 		if (!hzOwner)
 			return;
 		
-		if (EntityUtils.IsPlayer(hzOwner))
+		int playerId = GetGame().GetPlayerManager().GetPlayerIdFromControlledEntity(hzOwner);
+		SCR_PlayerController playerController =  SCR_PlayerController.Cast(GetGame().GetPlayerManager().GetPlayerController(playerId));
+		if (!playerController)
+			return;
+		
+		//Print(string.Format("GRAD Undead - isPossessing: %1", playerController.IsPossessing()), LogLevel.NORMAL);
+		
+		if (EntityUtils.IsPlayer(hzOwner) && !playerController.IsPossessing())
 		{
 			if(GetHealthScaled() < 0.01)
 			{
